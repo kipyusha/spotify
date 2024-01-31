6ce01f92531ca030e7e061885882fa0c9199562e
kipyusha <kirya16garchev@gmail.com>
2024-01-31 18:44:26 +0600

    add list songs and search page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "inxnfpsryyvkckxtotfq.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
